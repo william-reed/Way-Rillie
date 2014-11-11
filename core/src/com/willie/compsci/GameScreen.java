88c634979ca3591fd702b6557307fe1699270bf2
William Reed <wreed58@bitbucket.org>
2014-11-10 22:28:23 -0500

    Added base for different screens

---
+++ core/src/com/willie/compsci/GameScreen.java
@@ -0,0 +1,55 @@
+package com.willie.compsci;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * The actual game screen
+ */
+public class GameScreen implements Screen {
+
+	private Game game;
+
+	public GameScreen(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
---
+++ core/src/com/willie/compsci/MenuScreen.java
@@ -0,0 +1,55 @@
+package com.willie.compsci;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * The main menu screen of the game
+ */
+public class MenuScreen implements Screen {
+
+	private Game game;
+
+	public MenuScreen(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
