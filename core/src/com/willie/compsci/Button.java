31171ea4bf518b759bb06c0fe4ff220129483e9d
William Reed <wreed58@bitbucket.org>
2014-11-11 11:10:10 -0500

    Base added for the menu to be adapted latter, along with Button class taken from Project Freeman

---
+++ core/src/com/willie/compsci/Button.java
@@ -0,0 +1,127 @@
+package com.willie.compsci;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * @author William Reed
+ * Last Edited: 11/28/13 11:30 A.M.
+ */
+public class Button {
+	
+	private Texture texture;
+	private Vector2 location;
+	private int width, height;
+	
+	/**
+	 * Defines a new Button
+	 * @param texture Texture to be drawn as this button
+	 * @param location The bottom left coordinates of the button
+	 */
+	public Button(Texture texture, Vector2 location){
+		this.texture = texture;
+		this.location = location;
+		
+		width = texture.getWidth();
+		height = texture.getHeight();
+	}
+	
+	/**
+	 * Checks if the mouse / finger is in the 
+	 * @return true if the Button is clicked
+	 */
+	public boolean isClicked(){
+		if(Gdx.input.isTouched()){ //do a quick refining
+			int inputY  = Gdx.graphics.getHeight() - Gdx.input.getY();
+			if(Gdx.input.getX() >  location.x && Gdx.input.getX() < location.x + width &&
+					inputY > location.y && inputY < location.y + height){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Draws the button at its location. 
+	 * @param batch SpriteBatch to be used to draw this.
+	 */
+	public void draw(SpriteBatch batch){
+		if(!isClicked()){
+			batch.draw(texture, location.x, location.y);
+		}
+	}
+
+	/**
+	 * Texture associated with this Button
+	 * @return the texture associated with this Button
+	 */
+	public Texture getTexture() {
+		return texture;
+	}
+
+	/**
+	 * Sets the texture involved with this Button
+	 * @param texture Texture to be set to
+	 */
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+	
+	/**
+	 * Location of the Button
+	 * @return Vector2 of the bottom left coordinates of the button
+	 */
+	public Vector2 getLocation() {
+		return location;
+	}
+	
+	/**
+	 * Sets the location of the button
+	 * @param location Coordinates of the button to be set to
+	 */
+	public void setLocation(Vector2 location) {
+		this.location = location;
+	}
+
+	/**
+	 * Gets the width of the button clickable region
+	 * @return the width of the button
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * Sets the width of the button clickable region
+	 * @param width Width to set the button to
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	/**
+	 * Gets the height of the button clickable region
+	 * @return the height of the Button 
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * Sets the height of the button clickable region
+	 * @param height
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	/**
+	 * Disposes of this button and its assets
+	 */
+	public void dispose() {
+		texture.dispose();
+	}
+	
+}
--- core/src/com/willie/compsci/MenuScreen.java
+++ core/src/com/willie/compsci/MenuScreen.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * The main menu screen of the game
@@ -11,24 +15,54 @@ import com.badlogic.gdx.graphics.GL20;
 public class MenuScreen implements Screen {
 
 	private Game game;
+	private Vector2 titlePos;
+	private SpriteBatch batch;
+	private Texture title;
+	private OrthographicCamera camera;
+	private Button playGameButton;
 
 	public MenuScreen(Game game) {
 		this.game = game;
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 		
-		//create camera and spritebatch
+		batch = new SpriteBatch();
+		title = new Texture(Gdx.files.internal("title.png"));
+		//gypsy calculations to center everything
+		titlePos = new Vector2(w / 2 - title.getWidth() / 2, h / 2 - title.getHeight() / 2 + 100);
+		
+		camera = new OrthographicCamera(w, h);
+		//translate camera so the bottom left pixel is (0,0)
+		camera.translate(w / 2, h / 2);
+		camera.update();
+		
+		playGameButton = new Button(new Texture(Gdx.files.internal("play.png")), new Vector2(w / 2 - 100, 100));
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//won't be needed if the camera is never moved
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		
-		//just need to draw a background, menu buttons, and probably a title
+		batch.begin();
+		{
+			batch.draw(title, titlePos.x, titlePos.y);
+			playGameButton.draw(batch);
+		}
+		batch.end();
+		
+		if(playGameButton.isClicked())
+		{
+			game.setScreen(new GameScreen(game));
+		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		//re-center menu buttons and a title
+		//TODO: re-center menu buttons and a title
 	}
 
 	@Override
@@ -53,7 +87,9 @@ public class MenuScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		//dispose of images used
+		batch.dispose();
+		title.dispose();
+		playGameButton.dispose();
 	}
 
 }
