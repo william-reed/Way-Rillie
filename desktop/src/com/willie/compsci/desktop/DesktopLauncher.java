9c9509f1cceacb01c5ba322e31e548044c5e4e53
William Reed <wreed58@bitbucket.org>
2014-11-10 21:55:04 -0500

    Innitial commit

---
+++ desktop/src/com/willie/compsci/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.willie.compsci.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.willie.compsci.WayRillie;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WayRillie(), config);
+	}
+}
---
+++ html/src/com/willie/compsci/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.willie.compsci.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.willie.compsci.WayRillie;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new WayRillie();
+        }
+}
