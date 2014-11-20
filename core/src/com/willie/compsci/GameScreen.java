09fced089f172a250b3f6fcd6bf2ea69ff3b6dcf
harrisonmg <gieraltowski.har@gmail.com>
2014-11-19 19:45:14 -0500

    continued work on implementing the unfinished character class

--- core/src/com/willie/compsci/GameScreen.java
+++ core/src/com/willie/compsci/GameScreen.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * The actual game screen
@@ -11,11 +15,25 @@ import com.badlogic.gdx.graphics.GL20;
 public class GameScreen implements Screen {
 
 	private Game game;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
 
 	public GameScreen(Game game) {
 		this.game = game;
-		Character duck = new Character(64, 64, 0, 0, 10, null)
-		//create camera and spritebatch
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		//create characters at opposite ends of screen
+		Character duck = new Character(64, 64, 0, 0, 10, null);
+		Character mojo = new Character(64, 64, w - 64, 0, 10, null);
+		
+		//create spritebatch and camera
+		batch = new SpriteBatch();
+		
+		camera = new OrthographicCamera(w, h);
+		//translate camera so the bottom left pixel is (0,0)
+		camera.translate(w / 2, h / 2);
+		camera.update();
 	}
 	
 	public void update(float delta) {
