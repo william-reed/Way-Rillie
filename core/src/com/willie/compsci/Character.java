7102ee56d414d601342f7b41dbaa22e55f96c665
harrisonmg <gieraltowski.har@gmail.com>
2014-11-19 21:12:38 -0500

    characters are now draw sans animation and can move. a basic control system was implemented as well as horizontal movement restrictions.

--- core/src/com/willie/compsci/Character.java
+++ core/src/com/willie/compsci/Character.java
@@ -1,16 +1,23 @@
 package com.willie.compsci;
 
+import java.util.HashMap;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Character
 {
-	private Rectangle rec;
+	private Rectangle rec = new Rectangle();
 	private final int MAX_HEALTH;
 	private int currentHealth;
-	private TextureRegion texture;
+	private HashMap<String, Integer> controls;
 	
-	public Character(float height, float width, float x, float y, int maxHealth, TextureRegion texture)
+	//will be changed to TextureRegion for use with a sprite sheet
+	private Texture texture;
+	
+	public Character(float height, float width, float x, float y, int maxHealth, Texture texture, HashMap<String, Integer> controls)
 	{
 		rec.height = height;
 		rec.width = width;
@@ -18,7 +25,61 @@ public class Character
 		rec.y = y;
 		MAX_HEALTH = maxHealth;
 		this.texture = texture;
+		this.controls = controls;
 	}
 	
+	public int getControl(String control)
+	{
+		return controls.get(control);
+	}
+	
+	public boolean canMoveLeft()
+	{
+		if (rec.x <= 0)
+			return false;
+		return true;
+	}
+	
+	public boolean canMoveRight()
+	{
+		if (rec.x + rec.width >= Gdx.graphics.getWidth())
+			return false;
+		return true;
+	}
+
+	public int getCurrentHealth()
+	{
+		return currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth)
+	{
+		this.currentHealth = currentHealth;
+	}
+	
+	public float getX()
+	{
+		return rec.x;
+	}
+	
+	public void setX(float x)
+	{
+		rec.x = x;
+	}
+	
+	public float getY()
+	{
+		return rec.y;
+	}
+	
+	public void setY(float y)
+	{
+		rec.y = y;
+	}
+	
+	public Texture getTexture()
+	{
+		return texture;
+	}
 	
 }
--- core/src/com/willie/compsci/GameScreen.java
+++ core/src/com/willie/compsci/GameScreen.java
@@ -1,8 +1,11 @@
 package com.willie.compsci;
 
+import java.util.HashMap;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,15 +20,35 @@ public class GameScreen implements Screen {
 	private Game game;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
+	private HashMap<String, Integer> player1Controls, player2Controls;
+	private Character duck, mojo;
+	private Character[] characters;
+	private final float WALK_SPEED;
 
 	public GameScreen(Game game) {
 		this.game = game;
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
+		//define controls (use yaml or something for this later?)
+		player1Controls = new HashMap<String, Integer>();
+		player1Controls.put("left", Keys.A);
+		player1Controls.put("right", Keys.D);
+		player1Controls.put("jump", Keys.W);
+		
+		player2Controls = new HashMap<String, Integer>();
+		player2Controls.put("left", Keys.LEFT);
+		player2Controls.put("right", Keys.RIGHT);
+		player2Controls.put("jump", Keys.UP);
+		
 		//create characters at opposite ends of screen
-		Character duck = new Character(64, 64, 0, 0, 10, null);
-		Character mojo = new Character(64, 64, w - 64, 0, 10, null);
+		duck = new Character(64, 64, 0, 0, 10, new Texture("red.png"), player1Controls);
+		mojo = new Character(64, 64, w - 64, 0, 10, new Texture("blue.png"), player2Controls);
+		characters = new Character[2];
+		characters[0] = duck;
+		characters[1] = mojo;
+		
+		WALK_SPEED = 5;
 		
 		//create spritebatch and camera
 		batch = new SpriteBatch();
@@ -45,13 +68,26 @@ public class GameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		//draw all the stuffs
-		//change for harrison
+		batch.begin();
+		batch.draw(duck.getTexture(), duck.getX(), duck.getY());
+		batch.draw(mojo.getTexture(), mojo.getX(), mojo.getY());
+		batch.end();
 	}
 
 	@Override
 	public void render(float delta) {
 		update(delta);
 		draw();
+		
+		//perform character control stuff
+		//TODO: player collision
+		for (Character c : characters)
+		{
+			if (Gdx.input.isKeyPressed(c.getControl("left")) && c.canMoveLeft())
+				c.setX(c.getX()-WALK_SPEED);
+			if (Gdx.input.isKeyPressed(c.getControl("right")) && c.canMoveRight())
+				c.setX(c.getX()+WALK_SPEED);
+		}
 	}
 
 	@Override
